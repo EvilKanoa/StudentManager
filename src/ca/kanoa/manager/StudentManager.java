1454c01745ebde732331b7c22b213682d59af860
EvilKanoa <kanoa@kanoa.ca>
2015-07-13 23:45:08 -0400

    updated look and feel

--- src/ca/kanoa/manager/StudentManager.java
+++ src/ca/kanoa/manager/StudentManager.java
@@ -2,6 +2,10 @@ package ca.kanoa.manager;
 
 import java.awt.EventQueue;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import ca.kanoa.manager.window.MainWindow;
 
 public class StudentManager {
@@ -9,12 +13,34 @@ public class StudentManager {
 	private static MainWindow main;
 	
 	public static void main(String[] args) {
+		try {
+		    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException ex) {
+			try {
+				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(-2);
+		}
 		main = new MainWindow();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				main.setVisible(true);
 			}
 		});
+	}
+	
+	public static String getLookAndFeelClassName(String nameSnippet) {
+	    LookAndFeelInfo[] plafs = UIManager.getInstalledLookAndFeels();
+	    for (LookAndFeelInfo info : plafs) {
+	        if (info.getName().equalsIgnoreCase(nameSnippet)) {
+	            return info.getClassName();
+	        }
+	    }
+	    return null;
 	}
 
 }
